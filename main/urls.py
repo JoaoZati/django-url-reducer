d8c4b051e0ccdd8df4186af38ded822af7687271
JoaoZati <joaozati@gmail.com>
2021-10-30 18:10:41 -0300

    Part #2: (start app reducer) - start simple slug post

--- main/urls.py
+++ main/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from reducer.views import redirect
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('<slug:slug>', redirect),
 ]
