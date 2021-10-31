d57e00f21ec550e96947d928b10a01ab42fa7ef2
JoaoZati <joaozati@gmail.com>
2021-10-31 10:44:29 -0300

    Part #3: (templates) - Add original link and reduced link with dinamic template;

--- reducer/views.py
+++ reducer/views.py
@@ -5,8 +5,16 @@ from reducer.models import UrlRedirect
 
 def redirect_url(request, slug):
     url_redirect = UrlRedirect.objects.get(slug=slug)
+
     return redirect(url_redirect.destiny)
 
 
 def reports(request, slug):
-    return render(request, 'reduce.html')
+    url_redirect = UrlRedirect.objects.get(slug=slug)
+    url_reduced = request.build_absolute_uri(f'/{slug}')
+    context = {
+        'reduce': url_redirect,
+        'url_reduced': url_reduced,
+    }
+
+    return render(request, 'reduce.html', context)
